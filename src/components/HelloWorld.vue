d1df5bbb3d6f2fd7059b9fa7119dfe4070b1a32c
Bright@huawei <brt2@qq.com>
2021-12-29 14:54:22 +0800

    集成Element Plus

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -27,6 +27,7 @@ const count = ref(0)
   </p>
 
   <button type="button" @click="count++">count is: {{ count }}</button>
+  <el-button type="primary">查询</el-button>
   <p>
     Edit
     <code>components/HelloWorld.vue</code> to test hot module replacement.
