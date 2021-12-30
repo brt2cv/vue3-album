5a24dd4b2c098cfa99c786081dd10169c3ef690e
Bright@huawei <brt2@qq.com>
2021-12-30 10:07:55 +0800

    相册小程序：上传图片

---
+++ src/components/Album.vue
@@ -0,0 +1,40 @@
+<template>
+  <h1> 相册小程序 </h1>
+
+  <van-uploader
+    multiple
+    v-model="fileList"
+    :after-read="afterRead"
+  />
+</template>
+
+<script setup>
+import { ref } from "vue"
+
+const fileList = ref([])
+console.log(`fileList: ${fileList}`)
+
+const afterRead = (file) => {
+    console.log(`file: ${file}`)
+}
+
+// export default {
+//     data() {
+//         return {
+//             fileList: []
+//         }
+//     },
+
+//     methods: {
+//         afterRead(file) {
+//             console.log(`file: ${file}`)
+//         }
+//     }
+// }
+</script>
+
+<style scoped>
+a {
+  color: #42b983;
+}
+</style>
